eafee12c7a43cc45870c55a6643b25346f0c1992
Oualid <jouhri.oualid@gmail.com>
2013-12-14 22:43:00 +0100

    fromat data

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -1,8 +1,5 @@
 class ReceiveTextController < ApplicationController
   def handlesms
-  	head :ok
-  	p params
-  	puts "HOHOHOHOOHOHOHO"
   	@alert = Alert.new
   	@alert.body = params[:Body]
   	@alert.to = params[:To]
@@ -10,7 +7,6 @@ class ReceiveTextController < ApplicationController
   	@alert.sms_satuts = params[:SmsStatus]
   	@alert.from = params[:From]
   	@alert.save
-  	p @alert
  #  	@twilio_client = Twilio::REST::Client.new "AC8157b2db1250f939730a35d1117015d8", "2e8c13201e1b7ac8c8967cdfb8da0aac"
  #  	@messages =  @twilio_client.account.messages.list()
  #  	m = @messages.each.first
