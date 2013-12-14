88d54cece140d6dbbb79d38293343fa1b5cb7b55
Oualid <jouhri.oualid@gmail.com>
2013-12-14 21:07:08 +0100

    save sms in db

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -7,6 +7,7 @@ class ReceiveTextController < ApplicationController
   	@alert.from_country = params[:FromCountry]
   	@alert.sms_satuts = params[:SmsStatus]
   	@alert.from = params[:From]
+  	@alert.save
  #  	@twilio_client = Twilio::REST::Client.new "AC8157b2db1250f939730a35d1117015d8", "2e8c13201e1b7ac8c8967cdfb8da0aac"
  #  	@messages =  @twilio_client.account.messages.list()
  #  	m = @messages.each.first
