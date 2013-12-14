6a36e968b9809cd3ead7ff295f509bae2ec02526
Oualid <jouhri.oualid@gmail.com>
2013-12-14 21:12:25 +0100

    add some puts

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -8,6 +8,7 @@ class ReceiveTextController < ApplicationController
   	@alert.sms_satuts = params[:SmsStatus]
   	@alert.from = params[:From]
   	@alert.save
+  	p @alert
  #  	@twilio_client = Twilio::REST::Client.new "AC8157b2db1250f939730a35d1117015d8", "2e8c13201e1b7ac8c8967cdfb8da0aac"
  #  	@messages =  @twilio_client.account.messages.list()
  #  	m = @messages.each.first
