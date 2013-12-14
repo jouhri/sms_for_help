5acbf73a529a498cec8c2e00a668a5d716162ec6
Oualid <jouhri.oualid@gmail.com>
2013-12-14 20:12:16 +0100

    connection to twilio api

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -1,5 +1,10 @@
 class ReceiveTextController < ApplicationController
   def handlesms
   	p params
+  	@twilio_client = Twilio::REST::Client.new "AC8157b2db1250f939730a35d1117015d8", "2e8c13201e1b7ac8c8967cdfb8da0aac"
+  	@messages =  @twilio_client.account.messages.list()
+  	m = @messages.each.first
+	m2 = @twilio_client.account.messages.get(m.sid)
+	# binding.pry
   end
 end
