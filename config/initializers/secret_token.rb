4dede504c25ca7cda6f62ba8b03ae02bbd7490c0
Oualid <jouhri.oualid@gmail.com>
2013-12-14 15:41:37 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmsForHelp::Application.config.secret_key_base = '13eb9713f2f75ae83f68ebc1f1398b4e61e80299bd63898489023d16d8f832bf9242e504b5c407bf7239c6de527148f28c4e82702de4f0a38388a7abd0a745f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SmsForHelp::Application.config.session_store :cookie_store, key: '_sms_for_help_session'
