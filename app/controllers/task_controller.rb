e3f342332a129cd72c1dc96a94de20acb6dd5129
Oualid <jouhri.oualid@gmail.com>
2013-12-15 01:04:04 +0100

    broadcast_message

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,6 +1,6 @@
 class TaskController < WebsocketRails::BaseController
 	def create
-		send_message :create_success, message, :namespace => :tasks
+		broadcast_message :create_success, message, :namespace => :tasks 
 	end
 
 end
